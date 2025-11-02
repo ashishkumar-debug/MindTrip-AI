8f2ef61a6acc1ac8c4b70bb1d0bd08f8b3980c04
Yash Suryan <suryanyash43@gamil.com>
2025-11-03 05:27:08 +0530

    Initial Commit

--- src/view-trip/components/Hotels.jsx
+++ src/view-trip/components/Hotels.jsx
@@ -8,16 +8,24 @@ function Hotels({ trip }) {
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
         {trip?.tripData?.hotels?.map((hotel, index) => (
-            <Link to={'https://www.google.com/maps/search/?api=1&query='+hotel.hotelName+","+hotel.address} target='_blank'>
-          <div className="hover:scale-110 transition-all cursor-pointer">
-            <img src="/hotel.jpg" className="rounded-lg" />
-            <div className="my-3 flex flex-col gap-2">
-              <h2 className="font-medium">{hotel.hotelName}</h2>
-              <h2 className="text-xs text-gray-500">📍 {hotel.address}</h2>
-              <h2 className="text-sm">💰 {hotel.price}</h2>
-              <h2 className="text-sm">⭐ {hotel.rating}</h2>
+          <Link
+            to={
+              "https://www.google.com/maps/search/?api=1&query=" +
+              hotel.hotelName +
+              "," +
+              hotel.address
+            }
+            target="_blank"
+          >
+            <div className="hover:scale-110 transition-all cursor-pointer">
+              <img src="/hotel.jpg" className="rounded-lg" />
+              <div className="my-3 flex flex-col gap-2">
+                <h2 className="font-medium">{hotel.hotelName}</h2>
+                <h2 className="text-xs text-gray-500">📍 {hotel.address}</h2>
+                <h2 className="text-sm">💰 {hotel.price}</h2>
+                <h2 className="text-sm">⭐ {hotel.rating}</h2>
+              </div>
             </div>
-          </div>
           </Link>
         ))}
       </div>
